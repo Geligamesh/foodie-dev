d642b314943e7c3d3cc3a790d8dcef26a4620e5d
Geligamesh <535758155@qq.com>
2020-01-14 23:12:09 +0800

    解决跨域问题

---
+++ foodie-api/src/main/java/com/gxb/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.gxb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        //添加cors配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //不介意使用*，不安全
+        config.addAllowedOrigin("http://localhost:8080");
+        //设置是否设置cookie信息
+        config.setAllowCredentials(true);
+        //设置允许请求的方式
+        config.addAllowedMethod("*");
+        //设置允许的header
+        config.addAllowedHeader("*");
+        //为url添加映射路径
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        //返回重新定义好的corsSource
+        return new CorsFilter(source);
+    }
+}
+
